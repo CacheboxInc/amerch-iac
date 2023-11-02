4a54735d924f449be005fc2dc64f3750e85d7b53
Artem Shakhov <artemonis@gmail.com>
2023-11-02 12:44:06 -0400

    renamed

--- ssh_key.tf
+++
@@ -1,5 +0,0 @@
-resource "ibm_is_ssh_key" "prod_ssh_key" {
-  name           = "${var.unique_id}1-ssh-key"
-  resource_group = ibm_resource_group.default_rg.id
-  public_key     = file("./ssh-pub-key")
-}
