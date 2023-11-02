07e26c2188cc1de5ead838cf1a92a0b01c95a054
Artem Shakhov <artemonis@gmail.com>
2023-11-02 12:43:43 -0400

    polishing

--- vsi.tf
+++ vsi.tf
@@ -7,10 +7,14 @@ resource "ibm_is_instance" "receiver-prod1" {
   vpc            = ibm_is_vpc.dr-vpc.id
   zone           = "${var.ibm_region}-1"
   resource_group = ibm_resource_group.default_rg.id
-  keys           = [ibm_is_ssh_key.prod_ssh_key.id]
+  keys           = [ibm_is_ssh_key.prod_dal_jumphost_ssh_key.id]
   image          = data.ibm_is_image.custom_image.id
   depends_on     = [ibm_is_security_group.receiver-sg]
   profile        = var.dr_reciver_host_profile
+  user_data      = <<-EOF
+                #!/bin/bash
+                systemctl start drsrv
+                EOF
   boot_volume {
     name = "${var.unique_id}1-receiver-server-bv"
     size = 250
@@ -28,10 +32,14 @@ resource "ibm_is_instance" "receiver-prod2" {
   vpc            = ibm_is_vpc.dr-vpc.id
   zone           = "${var.ibm_region}-2"
   resource_group = ibm_resource_group.default_rg.id
-  keys           = [ibm_is_ssh_key.prod_ssh_key.id]
+  keys           = [ibm_is_ssh_key.prod_dal_jumphost_ssh_key.id]
   image          = data.ibm_is_image.custom_image.id
   depends_on     = [ibm_is_security_group.receiver-sg]
   profile        = var.dr_reciver_host_profile
+  user_data      = <<-EOF
+                #!/bin/bash
+                systemctl start drsrv
+                EOF
   boot_volume {
     name = "${var.unique_id}1-receiver-server-bv2"
     size = 250
@@ -49,10 +57,14 @@ resource "ibm_is_instance" "receiver-prod3" {
   vpc            = ibm_is_vpc.dr-vpc.id
   zone           = "${var.ibm_region}-3"
   resource_group = ibm_resource_group.default_rg.id
-  keys           = [ibm_is_ssh_key.prod_ssh_key.id]
+  keys           = [ibm_is_ssh_key.prod_dal_jumphost_ssh_key.id]
   image          = data.ibm_is_image.custom_image.id
   depends_on     = [ibm_is_security_group.receiver-sg]
   profile        = var.dr_reciver_host_profile
+  user_data      = <<-EOF
+                #!/bin/bash
+                systemctl start drsrv
+                EOF
   boot_volume {
     name = "${var.unique_id}1-receiver-server-bv3"
     size = 250
