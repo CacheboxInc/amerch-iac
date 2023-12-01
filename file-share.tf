a4db10e1f7108641460cf46ac154432feca059c7
Artem Shakhov <artemonis@gmail.com>
2023-12-01 15:01:40 -0500

    added file-share.tf but it dowsn't work

---
+++ file-share.tf
@@ -0,0 +1,8 @@
+resource "ibm_is_share" "file-share" {
+  access_control_mode = "security_group"
+  name                = "${var.unique_id}1-file-share"
+  size                = 100
+  profile             = "dp2"
+  zone                = "us-east-1"
+  iops                = 6000
+}
